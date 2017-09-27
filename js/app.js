c725610cde8d7eaa2098764e1669156cedd3b0c7
Casey Hagen <clopezhagen@gmail.com>
2017-09-27 18:14:42 +0900

    Added HTML stuffs, playing around with list groups

--- js/app.js
+++ js/app.js
@@ -1,4 +1,22 @@
-$(document).ready(function(){
-  // Insert cool jQuery stuff here.
-  
-});
+// Groups 
+// var groupOne   =  [["panda_bearry", 1], ["blue elsa", 5], ["duhkawaiipanda", 3], 
+//                   ["mariakawaiistudio", 3], ["patricia", 3], ["chazzerae", 4]];
+
+// var groupTwo   =  [["duhkawaiipanda", 1], ["chazzerae", 2], ["totallytida", 14], 
+//                   ["ventanablanca", 1]];
+// var groupThree =  [["panda_bearry", 3], ["blueelsa", 5], ["totallytida", 7], 
+//                   ["duhkawaiipanda", 1], ["ventanablanca", 2]];
+
+// Prizes
+
+
+var prizeOne   = [25, 42, 32, 65, 1, 13, 15, 14, 17, 20, 44, 56, 76, 23, 21, 12];
+var prizeTwo   = [25, 42, 32, 65, 2, 13, 15, 14, 17, 20, 44, 56, 76, 23, 21, 12];
+var prizeThree = [25, 42, 32, 65, 3, 13, 15, 14, 17, 20, 44, 56, 76, 23, 21, 12];
+
+// Let's get it.
+
+function getPrize() {
+  var prize = prizeOne.shift(); // Each time getPrize() runs, takes off one array until undefined. 
+  alert(prize);
+}
